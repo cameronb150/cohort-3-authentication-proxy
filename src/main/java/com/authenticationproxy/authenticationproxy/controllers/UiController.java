5b8e83f902fc7bd180940e1591946892dd749412
JoshHaase0 <joshua.m.haase@gmail.com>
2023-10-27 11:00:01 -0500

    controller redirects to login by default

--- src/main/java/com/authenticationproxy/authenticationproxy/controllers/UiController.java
+++ src/main/java/com/authenticationproxy/authenticationproxy/controllers/UiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UiController {
     @GetMapping("/")
     public String ui() {
-        return "forward:/index.html";
+        return "forward:/login";
     }
     @GetMapping("/profile")
     public String profile() {
